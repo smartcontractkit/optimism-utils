347b4510ff91d90b1d14a581e0a6ce322a6ba5bf
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2021-04-13 10:00:50 +0200

    Fix run-ci.sh path

--- scripts/run-ci.sh
+++ scripts/run-ci.sh
@@ -6,7 +6,9 @@ function build_images() {
   docker-compose build --parallel -- deployer dtl batch_submitter relayer
 }
 
+current_dir=$(dirname "$0")
+
 git clone https://github.com/ethereum-optimism/optimism.git
 cd optimism/ops
 build_images
-docker-compose up -d && ./wait-for-sequencer.sh
+docker-compose up -d && ../../${current_dir}/wait-for-sequencer.sh
